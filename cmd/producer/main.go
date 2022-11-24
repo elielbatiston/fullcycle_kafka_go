32064f189ba54be198e07775ce919ec344700936
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-11-24 20:00:15 -0300

    feat: criando e configurando producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
 	fmt.Println("Hello GO")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap-servers": "fullcycle_kafka_go_kafka_1:9092",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
 }
